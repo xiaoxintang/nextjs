7b92ca7e6d065a4eb6629df824520aa2ca48281d
guiyuan <guiyuan@dianjia.io>
2025-12-30 14:00:41 +0800

    style: 样式优化

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,16 @@
-import Image from "next/image";
-import axios from "axios";
 import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
-  const res = await axios.get(
-    "https://shop-test.dianplus.cn/rs/ent/account/get_account_page.do",
-    {
-      params: {
-        mobilePhone: "15707971810",
-        entId: "10080",
-      },
-      headers: {
-        Cookie: "JSESSIONID=YzUwZTMxNTgtM2E5ZC00MTM5LTlkZGEtMGM2ZmJkMDUwM2Jl",
-      },
-    }
-  );
-
   return (
     <div>
-      <Link href="/uuid">uuid</Link>
-      <Link href="/login">登录</Link>
-      <Link href="/blog">博客</Link>
+      <div className="flex items-center h-5 gap-2">
+        <Link href="/uuid">uuid</Link>
+        <Separator orientation="vertical" />
+        <Link href="/login">登录</Link>
+        <Separator orientation="vertical" />
+        <Link href="/blog">博客</Link>
+      </div>
     </div>
   );
 }
